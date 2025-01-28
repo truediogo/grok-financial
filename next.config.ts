b2600c7e13f8286635f0edffc4efa25cb3577ea4
Diogo Antonio <diogouzu@gmail.com>
2025-01-28 19:40:40 -0300

    First commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'abs.twimg.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
